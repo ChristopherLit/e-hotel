e9ede1d1fea68ebf4886ac5b59640356a6a24a52
feftywacky <linfeiyu14@gmail.com>
2024-03-26 18:05:26 -0400

    updated db data

--- src/db_init_queries/sample_data.sql
+++ src/db_init_queries/sample_data.sql
@@ -1,4 +1,5 @@
 -- test data for hotel_chain table
 INSERT INTO hotel_chain (office_address, email, phone_number, name)
 VALUES ('1234 Main St, Ottawa, ON, A1A 1A1', 'temp@gmail.com', 2175551234, 'Springfield Hotels'),
-       ('134 ASDn St, Ottawa, ON, A1A W21', 'temp2@gmail.com', 2203480494, 'ASLDKJ Hotels');
+       ('134 ASDn St, Ottawa, ON, A1A W21', 'temp2@gmail.com', 2203480494, 'ASLDKJ Hotels'),
+       ('12 ave, Ottawa, ON, A1A 2S1', 'temp3@gmail.com', 2175000234, 'temp3 Hotels');
